e2ac36539d7b5bc2c7a4af33e3e1e1f1e1e69621
Weronika Ruszel <weronika.ruszel@host.com>
2021-05-04 01:06:14 +0200

    update

--- app/src/Controller/HelloWorldController.php
+++ app/src/Controller/HelloWorldController.php
@@ -5,20 +5,37 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class HelloWorldController.
+ *
+ * @Route("/hello-world")
  */
-class HelloWorldController
+class HelloWorldController extends AbstractController
 {
     /**
      * Index action.
      *
+     * @param string $name User input
+     *
      * @return \Symfony\Component\HttpFoundation\Response HTTP response
+     *
+     * @Route(
+     *     "/{name}",
+     *     methods={"GET"},
+     *     name="hello-world_index",
+     *     defaults={"name":"World"},
+     *     requirements={"name": "[a-zA-Z]+"},
+     * )
      */
-    public function index(): Response
+    public function index(string $name): Response
     {
-        return new Response('Hello World!');
+        return $this->render(
+            'hello-world/index.html.twig',
+            ['name' => $name]
+        );
     }
 }
